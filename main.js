cbd2c68a54f3aac473a01e5cec50267bfe898a62
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-25 12:31:28 +0200

    aggiunto memebri del team in un array di oggetti

---
+++ main.js
@@ -0,0 +1,34 @@
+// array di oggetti memmbri del team
+const peppozTeam = [
+    // index 0
+    {
+        nome: "Peppino",
+        ruolo: "Ceo e Founder",
+        foto: "fotodainseriredopodiognunodiloro"
+    },
+    // index 1
+    {
+        nome: "Angelo",
+        ruolo: "Chef Pasticcere",
+        foto: "fotodainseriredopodiognunodiloro"
+    },
+    // index 2
+    {
+        nome: "Rino",
+        ruolo: "Chef Rosticcere e Su-Chef Pasticcere",
+        foto: "fotodainseriredopodiognunodiloro"
+    },
+    // index 3
+    {
+        nome: "Maria",
+        ruolo: "Barista",
+        foto: "fotodainseriredopodiognunodiloro"
+    },
+    // index 4
+    {
+        nome: "Marino",
+        ruolo: "Barista",
+        foto: "fotodainseriredopodiognunodiloro"
+    },
+];
+
